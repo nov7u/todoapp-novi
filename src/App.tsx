0f5d5025326a75686eae50a997677d8966577864
nov7u <novi757575@gmail.com>
2021-11-12 10:24:35 +0700

    feat: editClick and prettier format

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,70 @@
-import React, { FC, ChangeEvent, useState } from 'react';
-import './App.css';
-import TodoTask from './Components/TodoTask';
-import { ITask } from './Interface';
+import React, { FC, ChangeEvent, useState} from "react"
+import "./App.css"
+import TodoTask from "./Components/TodoTask"
+import { ITask } from "./Interface"
 
 const App: FC = () => {
+   const [task, setTask] = useState<string>("")
+   const [todoList, setTodoList] = useState<ITask[]>([])
 
-  const [task, setTask] = useState<string>("");
-  const [todoList, setTodoList] = useState<ITask[]>([]);
-  
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.name === "task"){
-      setTask(event.target.value)
-    }
+   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+      if (event.target.name === "task") {
+         setTask(event.target.value)
+      }
+   }
 
-  };
+   const addTask = (): void => {
+      const newTask = { taskName: task }
+      setTodoList([...todoList, newTask])
+      setTask("");
+      console.log(todoList)
+   }
 
-  const addTask = (): void => {
-    const newTask = {taskName: task};
-    setTodoList([...todoList, newTask]);
-    // setTodoList("");
-    console.log(todoList);
-  };
+   const deleteTask = (taskNameToDelete: string): void => {
+      setTodoList(
+         todoList.filter((task) => {
+            return task.taskName !== taskNameToDelete
+         })
+      )
+   }
 
-  const deleteTask = (taskNameToDelete:string): void => {
-    setTodoList(todoList.filter((task) => {
-      return task.taskName !== taskNameToDelete
-    }))
-  }
+   const handleEdit = (editValue: string, id: number): void => {
+    const newTodoList = [...todoList]
+    newTodoList.forEach((task, index) =>{
+      if(index === id){
+        task.taskName = editValue
+      }
+    })
+    setTodoList(newTodoList)
+ }
 
-  return (
-    <div className="App">
-      <div className="header">
-        <div className="input-container">
-          <input 
-            type="text" 
-            placeholder="Input" 
-            name="task"
-            value={task}
-            onChange={handleChange} 
-          />
-        </div>
-        
-        <button onClick={addTask}>Submit</button>
+   return (
+      <div className="App">
+         <div className="header">
+            <div className="input-container">
+               <input
+                  type="text"
+                  placeholder="Input"
+                  name="task"
+                  id={task}
+                  value={task}
+                  onChange={handleChange}
+               />
+            </div>
+
+            <button onClick={addTask}>Submit</button>
+         </div>
+         <div className="todoList">
+            {todoList.map((task: ITask, index: number) => {
+               return (
+                  <TodoTask key={index} id={index} task={task} deleteTask={deleteTask}
+                  handleEdit={handleEdit}
+                  />
+               )
+            })}
+         </div>
       </div>
-      <div className="todoList">
-        {todoList.map((task: ITask, index: number) => {
-          return <TodoTask key={index} task={task} deleteTask={deleteTask} />;
-        })}
-      </div>
-    </div>
-  );
+   )
 }
 
-export default App;
+export default App
--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -1,31 +1,79 @@
-import React, {FC} from "react"; 
-import {ITask} from "../Interface";
+import React, { FC, useState } from "react";
+import { ITask } from "../Interface";
 
-interface Props{
-    task: ITask;
-    deleteTask(taskNameToDelete: string): void;
+interface Props {
+   task: ITask;
+   id: number;
+   deleteTask: (taskNameToDelete: string) => void;
+   handleEdit: (editValue: string, id: number) => void;
 }
 
-const TodoTask: FC<Props> = ({task, deleteTask}: Props) => {
-    
-    const deleteClick = () => {
-        deleteTask(task.taskName)
-    }
-    
-    return (
-    <div className="task">
-        <div className="content">
-           <span>{task.taskName}</span>
-        </div>
-        <button 
-        onClick={deleteClick}
-        >
-        X
-        </button>
-    </div>
-    );
+const TodoTask: FC<Props> = ({ task, id, deleteTask, handleEdit }) => {
+   const [editTask, setEditTask] = useState(false);
+   const [editValue, setEditValue] = useState(task.taskName);
+
+   const deleteClick = () => {
+      deleteTask(task.taskName);
+   };
+
+   //useEffect
+   //FetchAPI
+   //ShadowHeader
+   const editCLick = () => {
+      setEditTask(true);
+   };
+
+   const saveClick = (id: number) => {
+       setEditTask(false)
+       if(editValue){
+           handleEdit(editValue, id)
+       }else{
+           setEditValue(task.taskName)
+       }
+   }
+
+   const cancelClick = () => {
+       
+   }
+
+   
+
+
+   if (editTask) {
+      return (
+         <li>
+            <input
+               type="text"
+               id="editValue" value={editValue} name="editValue"
+               onChange={e => setEditValue(e.target.value.toLowerCase())}
+            />
+
+            <button
+            onClick={() => saveClick (id)}
+            >
+               Save
+            </button>
+
+            <button
+            onClick={() => cancelClick}
+            >
+                Cancel
+            </button>
+         </li>
+      );
+   } else {
+      return (
+         <div className="task">
+            <div className="content">
+               <span>{task.taskName}</span>
+            </div>
+            <button onClick={editCLick}>Edit</button>
+            <button onClick={deleteClick}>Delete</button>
+         </div>
+      );
+   }
 };
 
-
-
 export default TodoTask;
+
+
